06a9fe1255d866811694e715274c932ffd9c084a
PARTH PRAJAPATI <parthy4399@gmail.com>
2020-08-18 02:17:47 +0530

    Add files via upload

---
+++ SJF(Shortest Job First)/SJFscript.js
@@ -0,0 +1,48 @@
+let rows = 0;
+
+/*
+This works well but not getting why entered values are getting reset/erased
+on adding a new joob by clicking the ADD JOB button
+function AddJob() {
+    let tab = document.querySelector("#Inputs");
+    ++rows;
+    tab.innerHTML +=
+        `
+        <tr>
+            <td><input type="text" value="Job-${rows}" id="J${rows}" size="2" disabled></td>
+            <td><input type="number" id="AT${rows}"></td>
+            <td><input type="number" id="BT${rows}"></td>
+            <td><input type="text" id="CT${rows}" size="5" disabled></td>
+            <td><input type="text" id="TAT${rows}" size="5" disabled></td>
+            <td><input type="text" id="WT${rows}" size="5" disabled></td>
+            <td><input type="text" id="RT${rows}" size="5" disabled></td>
+        </tr>
+    `
+}
+*/
+function AddJob() {
+    let Row = document.querySelector("#Inputs").insertRow(++rows);
+    let col = -1;
+    let cell = Row.insertCell(++col);
+    cell.innerHTML += `<input type="text" value="Job-${rows}" id="J${rows }" size="2" disabled>`;
+    cell = Row.insertCell(++col);
+    cell.innerHTML += `<input type="number" id="AT${rows}">`;
+    cell = Row.insertCell(++col);
+    cell.innerHTML += `<input type="number" id="BT${rows}">`;
+    cell = Row.insertCell(++col);
+    cell.innerHTML += `<input type="text" id="CT${rows}" size="5" disabled>`;
+    cell = Row.insertCell(++col);
+    cell.innerHTML += `<input type="text" id="TAT${rows}" size="5" disabled>`;
+    cell = Row.insertCell(++col);
+    cell.innerHTML += `<input type="text" id="WT${rows}" size="5" disabled>`;
+    cell = Row.insertCell(++col);
+    cell.innerHTML += `<input type="text" id="RT${rows}" size="5" disabled>`;
+}
+
+function RemoveJob() {
+    document.querySelector("#Inputs").deleteRow(rows--);
+}
+
+function Solve() {
+    // incomplete
+}
